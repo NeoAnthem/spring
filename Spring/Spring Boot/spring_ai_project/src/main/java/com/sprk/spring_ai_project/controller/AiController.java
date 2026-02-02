76af5c58142b612e48ae055e02ea12005ed795c9
Darshan Takarkhede <100999462+NeoAnthem@users.noreply.github.com>
2026-02-02 13:37:52 +0530

    Add files via upload

---
+++ Spring/Spring Boot/spring_ai_project/src/main/java/com/sprk/spring_ai_project/controller/AiController.java
@@ -0,0 +1,23 @@
+package com.sprk.spring_ai_project.controller;
+
+import lombok.AllArgsConstructor;
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/Ai")
+public class AiController {
+    private final ChatClient chatClient;
+    public AiController(ChatClient chatClient) {
+        this.chatClient = chatClient;
+
+    }
+    @GetMapping("/ask")
+    public String askAi(@RequestParam String message){
+        return chatClient.prompt().user(message).call().content();
+    }
+
+}
